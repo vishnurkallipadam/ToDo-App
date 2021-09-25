e5bc54c1b73e606ccfa73de8f5342f39583cea17
Vishnu Mohan V K <vishnumohanvk@gmail.com>
2021-09-25 19:33:22 +0530

    Final Version

--- js/script.js
+++ js/script.js
@@ -1,25 +1,24 @@
-const form=document.getElementById("form1");
-const username=document.getElementById("username");
+function Validate(){
+    let user = document.getElementById("username");
+    let pass = document.getElementById("password");
+    return userCheck(user,pass,successLogin,errorLogin)
+}
 
-const password1=document.getElementById("password");
-
-
-form.addEventListener('submit',(event)=>{
-    event.preventDefault();
-    checkInputs();
-});
-function checkInputs(){
-    const usernameValue=username.value.trim();
-    
-    const password1Value= password1.value.trim();
-    
-   
-    if(usernameValue=="admin"&&password1Value=="12345"){
-        form.setAttribute('action','todo.html');
-        form.submit();
-        
-    }else{
-        alert("invalid username or password")
+function userCheck(user,pass,successLogin,errorLogin){
+    if(user.value == "admin" && pass.value == "12345"){
+        return successLogin();
     }
-    
+    else{
+        return errorLogin();
+    }
+}
+function successLogin(){
+        
+        return true;
+
+
+}
+function errorLogin(){
+  alert("invalid username and password")
+    return false;
 }
